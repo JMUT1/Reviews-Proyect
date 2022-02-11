b08024934565e14036874d12c841beac348f73ba
chemaT <chemaaa94@gmail.com>
2022-02-11 16:02:05 -0600

    fix error on nextBTN add EventLister

--- app.js
+++ app.js
@@ -70,5 +70,8 @@ function showPerson(person){
 
 nextBtn.addEventListener("click", function(){
   currentItem++;
+  if(currentItem > reviews.length - 1){
+    currentItem = 0;
+  }
   showPerson(currentItem);
 })
